2595fef6717e282a763e805f972d632d429d0263
Kautuk <kautukkt98@gmail.com>
2019-07-12 22:06:32 +0530

    Hello new user

--- server/server.js
+++ server/server.js
@@ -9,20 +9,39 @@ const port = process.env.PORT || 8000;
 var app = express();
 var server = http.createServer(app);
 var io = socketIO(server);
-
+ 
 
 app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
     console.log('New user connected');
 
+    socket.emit('newMessage',{
+            from: 'Admin',
+            text: 'welcome to the chat app',
+            createdAt: new Date().getTime()
+    });
+
+    socket.broadcast.emit('newMessage', {
+            from: 'admin',
+            text: 'new user joined',
+            createdAt: new Date().getTime()
+    });
+
     socket.on('createMessage',(cmsg) => {
-        console.log('Created Message', cmsg);
+        console.log('Created Message', cmsg); 
         io.emit('newMessage',{
             from: cmsg.from,
             text: cmsg.text,
             createdAt: new Date().getTime()
         });
+
+        // Broadcasting
+        // socket.broadcast.emit('newMessage', {
+        //     from: cmsg.from,
+        //     text: cmsg.text,
+        //     createdAt: new Date().getTime()
+        // });
     });
     
     socket.on('disconnect', ()=> {
