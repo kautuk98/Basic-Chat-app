34f2bf8d01a595583a5ac53c54269c3f1aaaf51a
Kautuk <kautukkt98@gmail.com>
2019-07-13 16:05:27 +0530

    Styling the chat app

--- public/js/index.js
+++ public/js/index.js
@@ -31,11 +31,14 @@ socket.on('newLocationMessage', function (message) {
 jQuery('#message-form').on('submit', function (e) {
     e.preventDefault();
 
+
+    var messageTextbox = jQuery('[name=message]');
     socket.emit('createMessage',{
         from: 'User',
-        text: jQuery('[name=message]').val()
+        text: messageTextbox.val()
     }, function (){
-
+        // to clear out the input
+        messageTextbox.val('')
     });
 });
 
@@ -46,12 +49,16 @@ locationButton.on('click', function () {
         return alert('Geolocation not supported by the browser');
     }
 
+    locationButton.attr('disabled','disabled').text('Sending Location..'); 
+
     navigator.geolocation.getCurrentPosition(function (position) {
+        locationButton.removeAttr('disabled').text('Send Location');
         socket.emit('createLocationMessage', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         });
     }, function () {
+        locationButton.removeAttr('disabled').text('Send Location');
         alert('Unable to fetch location');
     });
 });
