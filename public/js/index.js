569a3c37cf7db1080984a83317f7002119d5c124
Kautuk <kautukkt98@gmail.com>
2019-07-13 15:27:04 +0530

    Geolocation

--- public/js/index.js
+++ public/js/index.js
@@ -8,13 +8,25 @@ socket.on('disconnect', function(){
     console.log('Disconnected from the server');
 });
 
-socket.on('newMessage', function (msg) {
-    console.log('New message', msg);
+socket.on('newMessage', function (message) {
+    console.log('New message', message);
     var li = jQuery('<li></li>');
-    li.text(`${msg.from}:    ${msg.text}`);
+    li.text(`${message.from}:    ${message.text}`);
 
     jQuery('#messages').append(li);
 });
+
+
+socket.on('newLocationMessage', function (message) {
+    var li = jQuery('<li></li>');
+    var a =jQuery('<a target="_blank">My Current Location</a>');
+
+    li.text(`${message.from}: `);
+    a.attr('href', message.url);
+    li.append(a);
+    jQuery('#messages').append(li);
+});
+
 
 jQuery('#message-form').on('submit', function (e) {
     e.preventDefault();
@@ -24,5 +36,22 @@ jQuery('#message-form').on('submit', function (e) {
         text: jQuery('[name=message]').val()
     }, function (){
 
+    });
+});
+
+var locationButton = jQuery('#send-location');
+locationButton.on('click', function () {
+    // Geolocation web api fetch
+    if(!navigator.geolocation){
+        return alert('Geolocation not supported by the browser');
+    }
+
+    navigator.geolocation.getCurrentPosition(function (position) {
+        socket.emit('createLocationMessage', {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        });
+    }, function () {
+        alert('Unable to fetch location');
     });
 });
